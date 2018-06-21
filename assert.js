4a1c9d4df44d4b79fc28c275f8fc153d7d849d85
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-06-20 19:38:14 -0500

    Add basic functionality

---
+++ assert.js
@@ -0,0 +1,79 @@
+export default class Assert{
+
+    
+    assertArray(expectation, result, test = ""){
+        if(expectation.length !== result.length){
+            return this.output(expectation, result, test, false);
+        }
+        let isSame = true;
+        for(let i = 0; i < expectation.length; i++){
+            if(expectation[i] !== result[i]){
+                isSame = false;
+                break;
+            }
+        }
+        return this.output(expectation, result, test, isSame);
+        
+    }
+    
+    assertObj(expectation, result, test = ""){
+        let isSame = true;
+        let keys1 = Object.keys(result);
+        let keys2 = Object.keys(expectation);
+        if(keys1.length !== keys2.length){
+            isSame = false;
+        }
+        for(let prop in result){
+         if(expectation[prop] !== result[prop]){
+             isSame = false;
+         }
+        }
+
+        return this.output(expectation, result, test, isSame)
+    }
+    
+    output(expectation, result, test, isSame){
+        if(isSame){
+            return `TEST [${test.toUpperCase}] PASSED`
+        }else{
+            if(typeof expectation === 'object'){
+                return `TEST ${test} FAILED EXPECTED ${JSON.stringify(expectation)} but got ${JSON.stringify(result)}`
+            }else{
+                return `TEST ${test} FAILED EXPECTED ${expectation} but got ${result}`
+            }
+        }
+    }
+    assert(expectation, result, test=""){
+        if(typeof expectation !== typeof result){
+            test += `TYPE DIFFERENCE ${typeof expectation} compared to ${typeof result}`
+            return this.output(expectation, result, test, false)
+        }
+        if(typeof expectation === "object"){
+            return this.assertObj(expectation, result, test)
+        } else if(Array.isArray(expectation)){
+            return this.assertArray(expectation, result, test);
+        } else{
+            let isSame = expectation === result ? true : false;
+            return this.output(expectation, result, test, isSame)
+        }
+    }
+    
+
+    
+}
+
+let assert = new Assert();
+let i = assert.assertArray([1,2,3], [1,2,3,4], "Arrays Equal");
+i
+
+let obj1 = {a: 1, b: 2, c: 3, d: null}
+let obj2 = {a: 1, b: 2, c: 3}
+let j = assert.assertObj(obj1, obj2)
+j
+
+let k = assert.assert(obj1, obj2, "Compare objects");
+k
+
+console.log(assert.assert("test", 5))
+console.log(assert.assert("pie", "pie", "Compare pies"));
+
